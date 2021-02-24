0b35038bfbc2e8b03be74e25a7dd08f80f27d8c5
Youming Xu <youming.xu@volvocars.com>
2021-02-24 14:56:48 +0800

    Initial commit

---
+++ app/src/main/java/com/volvocars/servicebestpractice/DownloadTask.java
@@ -0,0 +1,150 @@
+package com.volvocars.servicebestpractice;
+
+import android.os.AsyncTask;
+import android.os.Environment;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class DownloadTask extends AsyncTask<String,Integer,Integer> {
+    public static final int TYPE_SUCCESS = 0;
+    public static final int TYPE_FAILED = 1;
+    public static final int TYPE_PAUSED = 2;
+    public static final int TYPE_CANCELED = 3;
+
+    private DownloadListener listener;
+    private boolean isCancelled;
+    private boolean isPaused;
+    private int lastProgress;
+
+    public DownloadTask(DownloadListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    protected Integer doInBackground(String[] params) {
+        InputStream is = null;
+        RandomAccessFile savedFile = null;
+        File file = null;
+
+        try {
+            long downloadLength = 0;
+            String downloadUrl = params[0];
+            String fileName = downloadUrl.substring(downloadUrl.lastIndexOf("/"));
+            String directory = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS).getPath();
+            file = new File(directory + fileName);
+            if (file.exists()) {
+                downloadLength = file.length();
+            }
+            long contentLength = getContentLength(downloadUrl);
+            if (contentLength == 0) {
+                return TYPE_FAILED;
+
+            } else if (contentLength == downloadLength) {
+                return TYPE_SUCCESS;
+            }
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .addHeader("RANGE","bytes=" + downloadLength + "-")
+                    .url(downloadUrl)
+                    .build();
+            Response response = client.newCall(request).execute();
+            if (response != null) {
+                is = response.body().byteStream();
+                savedFile = new RandomAccessFile(file, "rw");
+                savedFile.seek(downloadLength);
+
+                byte[] b = new byte[1024];
+                int total = 0;
+                int len;
+                while ((len = is.read(b)) != -1) {
+                    if (isCancelled) {
+                        return TYPE_CANCELED;
+                    } else if (isPaused) {
+                        return TYPE_PAUSED;
+                    } else {
+                        total += len;
+                        savedFile.write(b, 0, len);
+                        int progress = (int) ((total + downloadLength) * 100 / contentLength);
+                        publishProgress(progress);
+                    }
+                }
+                response.body().close();
+                return TYPE_SUCCESS;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+                if (savedFile != null) {
+                    savedFile.close();
+                }
+                if (isCancelled && file != null) {
+                    file.delete();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return TYPE_FAILED;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer[] values) {
+        int progress = values[0];
+        if (progress > lastProgress) {
+            listener.onProgress(progress);
+            lastProgress = progress;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Integer status) {
+        switch (status) {
+            case TYPE_SUCCESS:
+                listener.onSuccess();
+                break;
+            case TYPE_FAILED:
+                listener.onFailed();
+            case TYPE_PAUSED:
+                listener.onPaused();
+            case TYPE_CANCELED:
+                listener.onCanceled();
+            default:
+                break;
+        }
+
+
+    }
+
+    public void pauseDownload() {
+        isPaused = true;
+    }
+
+    public void cancelDownload() {
+        isCancelled = true;
+    }
+
+    private long getContentLength(String downloadUrl) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(downloadUrl)
+                .build();
+        Response response = client.newCall(request).execute();
+        if (response != null && response.isSuccessful()) {
+            long contentLength = response.body().contentLength();
+            response.body().close();
+        }
+        return 0;
+    }
+}
